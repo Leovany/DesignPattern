ba0037eff3261638dcd1caab9194440fe1d778b9
Leovany <348083098@qq.com>
2024-05-12 18:55:11 +0800

    设计模式

---
+++ src/com/leovany/designpattern/Test.java
@@ -0,0 +1,14 @@
+package com.leovany.designpattern;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("==");
+        // 中国
+        System.out.println("w我们");
+        // 哈哈
+        System.out.println("还可以");
+        // 怎么可以这样
+
+
+    }
+}
